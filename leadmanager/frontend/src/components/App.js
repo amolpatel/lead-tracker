e2ec522316de1b05e9a2a4de3a838b250fdf264f
amolpatel <amolpatel4@gmail.com>
2019-02-24 23:08:20 -0500

    added redux store, created leads table, hooked up get all leads

--- leadmanager/frontend/src/components/App.js
+++ leadmanager/frontend/src/components/App.js
@@ -4,15 +4,20 @@ import ReactDOM from "react-dom";
 import Header from "./layout/Header";
 import Dasboard from "./leads/Dashboard";
 
+import { Provider } from "react-redux";
+import store from "../store";
+
 class App extends Component {
   render() {
     return (
-      <Fragment>
-        <Header />
-        <div className="container">
-          <Dasboard />
-        </div>
-      </Fragment>
+      <Provider store={store}>
+        <Fragment>
+          <Header />
+          <div className="container">
+            <Dasboard />
+          </div>
+        </Fragment>
+      </Provider>
     );
   }
 }
